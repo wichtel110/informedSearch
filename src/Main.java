8cb41e12c0343a91cdeb999c994ce2fd8e25b3de
Michael Heide <mail@heidemichael.de>
2018-12-02 17:59:44 +0100

    Added Secon Heuristic(Manhatten Heuristic)

--- src/Main.java
+++ src/Main.java
@@ -21,10 +21,12 @@ public class Main {
             Node currentNode = pq.remove();
             currentNode.expandMoves();
             counter++;
+
             for(int i = 0; i< currentNode.getChildList().size(); i++){
                 Node currentChild = currentNode.getChildList().get(i);
-                calcHeuristicValue(currentChild,goalArr);
-                if(!checkNodeForGoal(currentChild,goalArr)){
+                calcSecondHeuristicValue(currentChild,goalArr);
+
+                if(!checkNodeForGoal(currentChild,goalArr) && !checkParentNode(currentChild,currentChild.getParent())){
                     pq.add(currentChild);
                 }else{
                     goalFound = true;
@@ -33,6 +35,20 @@ public class Main {
                 }
             }
         }
+    }
+
+    public static boolean checkParentNode(Node currentChild, Node parent) {
+        boolean isSame = false;
+        int current[] = currentChild.getPuzzle();
+
+        for(int i = 0; i < current.length; i++){
+            if(current[i] != parent.getPuzzle()[i]){
+                return false;
+            }else {
+                isSame = true;
+            }
+        }
+        return isSame;
     }
 
     private static boolean checkNodeForGoal(Node currentChild, int[] goalArr) {
@@ -50,14 +66,30 @@ public class Main {
     }
 
 
-    public static void calcHeuristicValue(Node node, int arr[])
-    {
+    public static void calcHeuristicValue(Node node, int goalArr[]) {
         int puzzle[] = node.getPuzzle();
         int counter = 0;
         for(int i = 0; i< puzzle.length; i++){
-           if(puzzle[i] != arr[i]){
+           if(puzzle[i] != goalArr[i]){
                counter += 1;
            }
+        }
+        node.setHeuristicValue(counter);
+    }
+
+    public static void calcSecondHeuristicValue(Node node, int goalArr[]){
+        int puzzle[] = node.getPuzzle();
+        int counter = 0,tempVal = 0;
+        for(int i = 0; i< puzzle.length; i++){
+            for(int f = 0; f< goalArr.length; f++){
+                if(puzzle[i] == goalArr[f]){
+                    tempVal = i-f;
+                    if(tempVal < 0){
+                        tempVal *= -1;
+                    }
+                }
+            }
+            counter += tempVal;
         }
         node.setHeuristicValue(counter);
     }
